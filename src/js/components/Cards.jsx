27f1cb5b9fa9085122fa4809844f74d4a7db7c40
jsteel17 <161091774+jsteel17@users.noreply.github.com>
2025-01-24 04:08:43 +0000

    First Commit

--- src/js/components/Cards.jsx
+++ src/js/components/Cards.jsx
@@ -1,4 +1,7 @@
 import React from "react";
+import pracImage from "../../img/prac.jpeg";
+import "bootstrap/dist/css/bootstrap.min.css";
+
 
 const Cards = () => {
     return (
@@ -6,7 +9,19 @@ const Cards = () => {
         <div className="card_container">
             <div className="card">
                 <div className="img_container">   
-                    <img src="./img/prac.jpeg"></img>
+                    <img src={pracImage}></img>
+                </div>
+                <div className="card_text">
+                    <h2 className="card_title">Card Title</h2>
+                    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Enim vitae ipsam itaque?</p>
+                </div>
+                <div className="btn_background">
+                    <button type="button" class="btn btn-primary">Find Out More!</button>
+                </div>
+            </div>
+            <div className="card">
+                <div className="img_container">
+                <img src={pracImage}></img>
                 </div>
                 <h2 className="card_title">Card Title</h2>
                 <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Enim vitae ipsam itaque?</p>
@@ -16,7 +31,7 @@ const Cards = () => {
             </div>
             <div className="card">
                 <div className="img_container">
-                <img src="./img/prac.jpeg"></img>
+                <img src={pracImage}></img>
                 </div>
                 <h2 className="card_title">Card Title</h2>
                 <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Enim vitae ipsam itaque?</p>
@@ -26,17 +41,7 @@ const Cards = () => {
             </div>
             <div className="card">
                 <div className="img_container">
-                <img src="./img/prac.jpeg"></img>
-                </div>
-                <h2 className="card_title">Card Title</h2>
-                <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Enim vitae ipsam itaque?</p>
-                <div className="btn_background">
-                    <button type="button" class="btn btn-primary">Find Out More!</button>
-                </div>
-            </div>
-            <div className="card">
-                <div className="img_container">
-                <img src="./img/prac.jpeg"></img>
+                <img src={pracImage}></img>
                 </div>
                 <h2 className="card_title">Card Title</h2>
                 <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Enim vitae ipsam itaque?</p>
